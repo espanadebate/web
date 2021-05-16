0f667caa4e7d750f3b82d1730c1654ad30c0e59b
gserranof <49020625+gserranof@users.noreply.github.com>
2021-05-16 17:40:51 +0200

    hotfix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "226feef38301521d2244044859a97c79",
-"/": "226feef38301521d2244044859a97c79",
-"main.dart.js": "7360fc071a6d6a528595e797d9c2c649",
+"index.html": "810fedefb88d926d7407a0d7de1afcd7",
+"/": "810fedefb88d926d7407a0d7de1afcd7",
+"main.dart.js": "6360b8670c4bb588322ed124f60f8698",
 "manifest.json": "b9ba0c68f2301160b01327784f03551b",
 "version.json": "b1093c30f71a930a709b6a881199183d"
 };
