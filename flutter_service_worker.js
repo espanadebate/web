c11da47f388e87486aeaab5159887848a204e31d
espanadebate <84279727+espanadebate@users.noreply.github.com>
2021-05-22 13:55:54 +0200

    hotfix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ba00d3425e94e8505e7f3caa01b4a61c",
-"/": "ba00d3425e94e8505e7f3caa01b4a61c",
-"main.dart.js": "241f2bbd438a0ac1cda32692be90d130",
+"index.html": "4154f9cf83670e8a231f11fe52b85e02",
+"/": "4154f9cf83670e8a231f11fe52b85e02",
+"main.dart.js": "fb5f17e83ef5094f8a4e918f49b6bb3e",
 "manifest.json": "b9ba0c68f2301160b01327784f03551b",
 "version.json": "b1093c30f71a930a709b6a881199183d"
 };
